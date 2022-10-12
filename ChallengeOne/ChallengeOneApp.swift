b29af5b273fc9c279af759e5e6d1eb91fc122b62
Muhammad Rizqullah Akbar <muhrizqullah@icloud.com>
2022-10-12 10:47:56 +0700

    Initial Commit

---
+++ ChallengeOne/ChallengeOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeOneApp.swift
+//  ChallengeOne
+//
+//  Created by Muhammad Rizqullah Akbar on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChallengeOne/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ChallengeOne
+//
+//  Created by Muhammad Rizqullah Akbar on 12/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
