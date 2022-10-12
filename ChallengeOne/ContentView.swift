2fb754b228b874b4df3a27968e18b1b97bcf0f04
Muhammad Rizqullah Akbar <muhrizqullah@icloud.com>
2022-10-12 11:27:39 +0700

    Finished

--- ChallengeOne/ContentView.swift
+++ ChallengeOne/ContentView.swift
@@ -8,14 +8,91 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State var valueOne: Double = 0.0
+    @State var unitOne: String = "Celcius"
+    @State var unitTwo: String = "Celcius"
+    @State var unit = ["Celcius", "Fahrenheit", "Kelvin"]
+    @FocusState var keyboardIsFocused: Bool
+    
+    var toCelcius: Double {
+        if unitOne == "Fahrenheit" {
+            let val = (valueOne - 32) * 5 / 9
+            return val
         }
-        .padding()
+        else if unitOne == "Kelvin" {
+            let val = valueOne - 273.15
+            return val
+        }
+        else {
+            return valueOne
+        }
+    }
+    
+    var result: Double {
+        if unitTwo == "Fahrenheit" {
+            let val = (toCelcius * 9 / 5) + 32
+            return val
+        }
+        else if unitTwo == "Kelvin" {
+            let val = toCelcius + 273.15
+            return val
+        }
+        else {
+            return toCelcius
+        }
+            
+    }
+    
+    var body: some View {
+        NavigationView{
+            Form {
+                Section {
+                    Picker("Units", selection: $unitOne) {
+                        ForEach(unit, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("UNIT")
+                }
+                
+                Section {
+                    Picker("Units", selection: $unitTwo) {
+                        ForEach(unit, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("UNIT")
+                }
+                
+                Section {
+                    TextField("Value", value: $valueOne, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($keyboardIsFocused)
+                } header: {
+                    Text("VALUE")
+                }
+                
+                Section {
+                    Text(result.formatted())
+                } header: {
+                    Text("Result")
+                }
+            }
+            .navigationTitle("Challenge One")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        keyboardIsFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
